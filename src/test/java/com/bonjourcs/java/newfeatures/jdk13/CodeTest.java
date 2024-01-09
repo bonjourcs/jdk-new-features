a1db124f3cf1ac42e47b6394abfa4234b0bf0948
Liang Chenghao <lch_office@foxmail.com>
2024-01-09 10:43:58 +0800

    feat: jdk13

---
+++ src/test/java/com/bonjourcs/java/newfeatures/jdk13/CodeTest.java
@@ -0,0 +1,37 @@
+package com.bonjourcs.java.newfeatures.jdk13;
+
+import com.bonjourcs.java.newfeatures.annotation.JDK;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * JDK13
+ *@author Liang Chenghao
+ *@date 2024/1/9
+ */
+@JDK(since = 13, lts = false)
+class CodeTest {
+
+    @Test
+    @DisplayName("Switch增强")
+    public void testSwitch() {
+        assertEquals(1, this.enhancedSwitch("1"));
+        assertEquals(2, this.enhancedSwitch("4"));
+        assertEquals(3, this.enhancedSwitch("8"));
+    }
+
+    private int enhancedSwitch(String desc) {
+        return switch (desc) {
+            case "1", "2", "3" -> 1;
+            case "4", "5", "6" -> 2;
+            default -> {
+                System.out.println("default approved...");
+                yield 3;
+            }
+        };
+    }
+}
